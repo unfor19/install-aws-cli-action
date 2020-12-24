c440720ab73c81fe019bbcbd989b28b111760f81
Meir Gabay <meir@prodops.io>
2020-12-24 02:23:04 +0200

    filter by name

--- entrypoint.sh
+++ entrypoint.sh
@@ -234,7 +234,8 @@ install_aws_cli(){
             cat <<EOT >> "${pwsh_file}.ps1"
 #!/usr/bin/env pwsh
 
-Get-CimInstance -Query 'SELECT Name FROM Win32_Product WHERE Name LIKE "AWS%"'
+Get-CimInstance -ClassName Win32_Product -Filter "Name='${aws_package_name}'"
+
 
 \$MSIArguments = @(
     "/i"
